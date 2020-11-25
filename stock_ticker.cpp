b8387d676b0abc03710fdbccef4dd92dc6fcc4eb
Chris Marino <chris.n.marino@gmail.com>
2020-11-25 18:45:46 -0500

    using fixes from marinoland_web to fetch the data from alphavantage

--- stock_ticker.cpp
+++ stock_ticker.cpp
@@ -1,23 +1,86 @@
+#include <memory>
+#include <map>
+#include <sstream>
+#include "RestClient.hpp"
 #include "ui_framework/MApplication.hpp"
 
-class MainApp : public MApplication {
+class SymbolEntryGui
+{
+public:
+	typedef std::shared_ptr<SymbolEntryGui> Ref;
+	static Ref Create(MGuiFactory* gui, MWindowRef parent)
+	{
+		Ref ref = std::make_shared<SymbolEntryGui>();
+		ref->button = gui->button(*parent, "Add");
+		ref->textbox = gui->textBox(*parent);
+
+		ref->button->OnClick([gui, ref]() {
+
+			std::stringstream url;
+			url << "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&apikey=KHMO1YK9QKQUFBFQ&symbol="
+				<< ref->textbox->GetText();
+			web::RestClient().get(url.str().c_str(),
+				std::map<std::string, std::string>(),
+				[gui, ref](web::RestWebResponse& response) {
+					std::stringstream str;
+					str << response.getCode();
+					gui->messageBox("Quote", str.str().c_str());
+					gui->messageBox("json", response.getBody().c_str());
+					response.getJson()->asObject(
+						[gui](json::JsonObjectNode& root)
+						{
+							root.get("Global Quote")->asObject(
+								[gui](json::JsonObjectNode& quote)
+								{
+									gui->messageBox("Quote", quote.get("05. price")->to_string().c_str());
+								}
+							);
+						}
+					);
+				}
+			);
+		});
+		return ref;
+	}
+	void Reposition(int x, int y, int width, int height)
+	{
+		textbox->SetPosition(4, y + 4);
+		button->SetPosition(width - 44, y + 4);
+
+		textbox->SetDimensions(width - 52, 20);
+		button->SetDimensions(40, 20);
+	}
+
+private:
+	MButtonRef button;
+	MTextBoxRef textbox;
+};
+
+class MainApp : public MApplication
+{
 public:
 	MainApp() : MApplication ("com.marinoland.testgui") {}
 	void Ready(MGuiFactory *gui) {
 
 		mainWindow = gui->window();
-		//buttonBox = gui->buttonBox(*mainWindow);
-		exitButton = gui->button(*mainWindow, "Clicky");
-
-		mainWindow->SetTitle("Super Window");
+		mainWindow->SetTitle("Marinoland Stock Ticker");
 		mainWindow->SetDimensions(400, 200);
 		mainWindow->Show();
-		exitButton->OnClick([&] () { mainWindow->Close(); });
+
+		symbolEntryGui = SymbolEntryGui::Create(gui, mainWindow);
+		Reposition();
+		MainApp *app = this;
+		mainWindow->OnResize([app](int w, int h) { app->Reposition(); });
+	}
+	void Reposition()
+	{
+		int width, height;
+		mainWindow->GetClientRect(width, height);
+		symbolEntryGui->Reposition(4, height - 28, width, 28);
 	}
 private:
+	std::shared_ptr<SymbolEntryGui> symbolEntryGui;
 	MWindowRef mainWindow;
-	//MButtonBoxRef buttonBox;
-	MButtonRef exitButton;
 };
 
 RUN(MainApp)
